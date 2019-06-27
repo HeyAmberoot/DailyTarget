a6165a63f5aba58888cab46ada12b8eb45b76dc2
HeyAmberoot <905902352@qq.com>
2019-06-27 15:08:29 +0800

    Initial Commit

---
+++ DailyTarget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyTarget
+//
+//  Created by xww on 2019/6/27.
+//  Copyright © 2019 amberoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
